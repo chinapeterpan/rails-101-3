03ebc2f262065274f618c8c08fc1e23dc7f3c884
chinapeterpan <232037776@qq.com>
2017-07-12 06:50:11 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,7 @@ class GroupsController < ApplicationController
 
     def create
         @group = Group.new(group_params)
+        @group = current_user
         if @group.save
             redirect_to groups_path
         else
